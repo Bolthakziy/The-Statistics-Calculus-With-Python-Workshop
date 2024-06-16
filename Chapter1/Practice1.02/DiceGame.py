b3c31cf2cf78a99bdda4fc49ab7d8fc463f44d0b
Alec K <boltakziy@gmail.com>
2024-06-16 22:36:02 +0900

    Dice Game

--- Chapter1/Practice1.02/DiceGame.py
+++ Chapter1/Practice1.02/DiceGame.py
@@ -1,1 +1,14 @@
+import random
 
+dice_value = random.randint(0, 7)
+
+while True :
+	guess = int(input("Enter your dice number. : "))
+
+	if (guess == dice_value) :
+		print("You find the exact dice number. Very good.")
+		break;
+	elif (guess > dice_value) :
+		print("Your answer is bigger than the dice value....")
+	else :
+		print("Your answer is lower than the dice value....")
