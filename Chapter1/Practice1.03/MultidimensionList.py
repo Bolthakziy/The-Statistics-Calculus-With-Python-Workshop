4bd50593171aeb8e90a83fbdd247e6ca6333d9b6
Alec K <boltakziy@gmail.com>
2024-06-18 23:47:01 +0900

    Multidimensional List

--- Chapter1/Practice1.03/MultidimensionList.py
+++ Chapter1/Practice1.03/MultidimensionList.py
@@ -1,1 +1,23 @@
+MultidimensionList = [[1, 2, 3], [1, 4, 9], [1, 8, 27]]
 
+print("First printing.")
+for row in MultidimensionList :
+	print(row[1])
+
+print("Second printing.")
+for row in MultidimensionList :
+	print(row)
+
+print("Third printing.")
+for row in MultidimensionList :
+	for element in row :
+		print(element)
+
+print("Forth printing.")
+for i in range(3) :
+	if i == 0 :
+		print(f"The {i + 1}st diagonal element : {MultidimensionList[i][i]}")
+	elif i == 1 :
+		print(f"The {i + 1}nd diagonal element : {MultidimensionList[i][i]}")
+	else :
+		print(f"The {i + 1}rd diagonal element : {MultidimensionList[i][i]}")
