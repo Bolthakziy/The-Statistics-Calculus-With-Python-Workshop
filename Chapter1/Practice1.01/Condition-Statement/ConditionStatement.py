8acab5c334480d98149a87233262ab03ddef30cd
Alec K <boltakziy@gmail.com>
2024-06-20 15:53:16 +0900

    Condition Statement

---
+++ Chapter1/Practice1.01/Condition-Statement/ConditionStatement.py
@@ -0,0 +1,18 @@
+x = int(input("Enter a number : "))
+
+if (x % 5 == 0) :
+	if (x % 7 == 0) :
+		print("x is divisible by 5 and 7.")
+	elif (x % 9 == 0) :
+		print("x is divisible by 5 and 9.")
+	else :
+		print("x is divisible by 5.")
+elif (x % 7 == 0) :
+	if (x % 9 == 0) :
+		print("x is divisible by 7 and 9.")
+	else :
+		print("x is divisible by 7.")
+elif (x % 9 == 0) :
+	print("x is divisible by 9.")
+else :
+	print("x is not divisible by 5, 7 and 9.")
