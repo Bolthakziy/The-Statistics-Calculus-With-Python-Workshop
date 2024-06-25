b88f9f6bd99abb73cf804dfcf72c44c56abe057f
Alec K <boltakziy@gmail.com>
2024-06-26 00:08:28 +0900

    Odd or Even

--- Chapter1/Practice1.04/IsItOdd/IsItOdd.py
+++ Chapter1/Practice1.04/IsItOdd/IsItOdd.py
@@ -1,1 +1,15 @@
+def is_It_Odd(number) :
+	if (number & 1) == 0 :
+		answer = False
+	else :
+		answer = True
 
+	return answer
+
+print("Let\'s play game!")
+num = int(input("Enter thr number you think, please! : "))
+
+if is_It_Odd(num) == False :
+	print("Your number is a even number.")
+else :
+	print("Your number is an odd number.")
