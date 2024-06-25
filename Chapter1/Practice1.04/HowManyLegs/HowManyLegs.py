5c2d873f0686a428ada3b28bad797bcb82dc2865
Alec K <boltakziy@gmail.com>
2024-06-25 23:52:42 +0900

    How many legs?

--- Chapter1/Practice1.04/HowManyLegs/HowManyLegs.py
+++ Chapter1/Practice1.04/HowManyLegs/HowManyLegs.py
@@ -1,1 +1,19 @@
+severalAnimals = {"Cat" : 4, "Bird" : 2, "Snake" : 0, "Ant" : 6, "Spider" : 8, "Squid" : 10, "Octopus" : 8}
 
+def printLegs(animalName) :
+	if severalAnimals[animalName] == 0 :
+		print(f"The {animalName} does not have legs.")
+	else :
+		print(f"The {animalName} has {severalAnimals[animalName]} legs.")
+
+while True :
+	stillWantToKnow = input("Do you want to know how many legs an Animal has? : ")
+
+	if (stillWantToKnow == 'Y') or (stillWantToKnow == 'y') :
+		whichAnimal = str(input("Which animal do you want? : "))
+		printLegs(whichAnimal)
+	elif (stillWantToKnow == 'N') or (stillWantToKnow == 'n') :
+		print("Goodbye!")
+		break;
+	else :
+		print("Please enter the correct animal\'s name!")
