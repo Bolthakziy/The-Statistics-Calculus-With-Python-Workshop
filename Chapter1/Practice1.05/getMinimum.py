718beb4b5debf3e982583c4a29d00023495b9dc8
Alec K <boltakziy@gmail.com>
2024-06-26 23:39:48 +0900

    Get Minimum Number.

--- Chapter1/Practice1.05/getMinimum.py
+++ Chapter1/Practice1.05/getMinimum.py
@@ -1,1 +1,22 @@
+def get_min(numberList) :
+	minIndex = 0
 
+	for idx, content in enumerate(numberList) :
+		if content < numberList[minIndex] :
+			minIndex = idx
+
+	return minIndex, numberList[minIndex]
+
+print("Please enter till 10 numbers!")
+print("Let\'s find the minimum number!")
+myList = []
+
+for i in range(0, 10) :
+	num = int(input("Enter your number till 10. : "))
+	myList.append(num)
+
+print("Entering works are done.")
+print("We find that the minimum number.")
+print("In a below tuple, first number is the index of the minimum number.")
+print("And second number is the minimum number.")
+print(get_min(myList))
