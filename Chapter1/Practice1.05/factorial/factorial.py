f05b5497fa7ac32a7033e7781f44e7b9f35aa8c6
Alec K <boltakziy@gmail.com>
2024-06-27 23:09:44 +0900

    Factorial by Recursion

--- Chapter1/Practice1.05/factorial/factorial.py
+++ Chapter1/Practice1.05/factorial/factorial.py
@@ -1,1 +1,11 @@
+def calculate_factorial(number) :
+	if number != 1 :
+		return number * calculate_factorial(number - 1)
+	else :
+		return number
 
+print("Let\'s learn the factorial calculation.")
+num = int(input("Please enter a number. : "))
+print("The factorial calculation is in progress.")
+print("Now the calculation is done. The answer is below!")
+print(calculate_factorial(num))
