18880b16b17134bc7a723a98a5ad50358ed3a1ad
Alec K <boltakziy@gmail.com>
2024-07-19 23:32:08 +0900

    Debuging

---
+++ Chapter1/Practice1.07/Debuging/debugingTest.py
@@ -0,0 +1,16 @@
+import pdb
+
+def multiply(a, b, c) :
+	return a * b * c
+
+x = (int)(input("Enter 1st number. : "))
+y = (int)(input("Enter 2nd number. : "))
+z = (int)(input("Enter 3rd number. : "))
+
+print("The multiple of above three numbers?")
+print(".....")
+print(".....")
+print(".....")
+print(multiply(x, y, z))
+pdb.set_trace()
+print("Goodbye!")
