fc2f682c2bd437d987220196bfdd4cdc80ceae2d
Alec K <boltakziy@gmail.com>
2024-08-09 23:44:36 +0900

    Normal Distribution

---
+++ Chapter2/Practice2.01/NormalDistribution/NormalDistribution.py
@@ -0,0 +1,8 @@
+### Normal distribution equals to Gaussian distribution. ###
+import numpy as np
+
+statisticsExample = np.random.normal(loc=100, scale=15, size=(3, 5))
+print("See an example of a statistics normal distribution.")
+print()
+print()
+print(statisticsExample)
