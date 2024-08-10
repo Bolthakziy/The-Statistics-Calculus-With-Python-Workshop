0e0d7bfbf0be85c5799dd2e7c5a305eeb22a2739
Alec K <boltakziy@gmail.com>
2024-08-10 23:36:04 +0900

    Poisson Distribution

---
+++ Chapter2/Practice2.01/PoissonDistribution/PoissonDistribution.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+PoissonExample = np.random.poisson(lam=10, size=(5, 5))
+print()
+print("See an example of a statistics Poisson distribution.")
+print()
+print()
+print(PoissonExample)
