192fcfc29ae43e31ca8166bb5ba554efc0a6d9f3
Alec K <boltakziy@gmail.com>
2024-09-09 23:57:30 +0900

    Pie Chart by Pandas

--- Chapter2/Practice2.04/PieChart_by_Pandas/PieChartByPandas.py
+++ Chapter2/Practice2.04/PieChart_by_Pandas/PieChartByPandas.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+
+studentsData = pd.DataFrame({
+		"name" : ["Alexander", "Laura", "Daniel", "David", "Nikolas", "Karen", "Alma"],
+		"gender" : ["male", "female", "male", "male", "male", "female", "female"],
+		"class" : ["Math", "Math", "Science", "Ethics", "Ethics", "Science", "Ethics"],
+		"GPA" : [92, 94, 95, 93, 91, 93, 98],
+		"classnumbers" : [3, 5, 3, 4, 5, 4, 3]
+		})
+studentsData["class"].value_counts().plot.pie()
+plt.show()
